ceba8a6d4010cfaf46421bce2ced48d8d5ae4a2e
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2024-03-14 02:54:25 +0100

    First stub

---
+++ MARC.pm
@@ -0,0 +1,61 @@
+package Plack::App::NKC::MARC;
+
+use base qw(Plack::Component);
+use strict;
+use warnings;
+
+use Plack::App::CPAN::Changes;
+use Plack::App::NKC::MARC::Output;
+use Plack::App::URLMap;
+use Plack::Session;
+use Plack::Util::Accessor qw(changes css data images lang tags);
+
+our $VERSION = 0.01;
+
+sub call {
+	my ($self, $env) = @_;
+
+	my $session = Plack::Session->new($env);
+
+	# Main application.
+	return $self->{'_urlmap'}->to_app->($env);
+}
+
+sub prepare_app {
+	my $self = shift;
+
+	$self->{'_data'} = $self->data;
+
+	my %p = (
+		'css' => $self->css,
+		'tags' => $self->tags,
+	);
+
+	my %common_params = (
+		%p,
+		'data' => $self->data,
+	);
+
+	my $app_output = Plack::App::NKC::MARC::Output->new(
+		%common_params,
+	)->to_app;
+	my $app_changes;
+	if (defined $self->changes) {
+		$app_changes = Plack::App::CPAN::Changes->new(
+			%p,
+			'changes' => $self->changes,
+		)->to_app;
+	}
+
+	$self->{'_urlmap'} = Plack::App::URLMap->new;
+	$self->{'_urlmap'}->map('/' => $app_output);
+	if (defined $self->changes) {
+		$self->{'_urlmap'}->map('/changes' => $app_changes);
+	}
+
+	return;
+}
+
+1;
+
+__END__
